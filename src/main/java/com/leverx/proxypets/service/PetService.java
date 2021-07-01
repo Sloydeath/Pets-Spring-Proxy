1fa1053d0922a097d9099f2c77c136e5f1d11a01
andrei.panas <panas.andreww@gmail.com>
2021-07-01 10:28:17 +0300

    adding service layer

---
+++ src/main/java/com/leverx/proxypets/service/PetService.java
@@ -0,0 +1,17 @@
+package com.leverx.proxypets.service;
+
+import com.leverx.proxypets.dto.pet.SwappingPetsDto;
+import com.leverx.proxypets.dto.pet.UpdatePetDto;
+import com.leverx.proxypets.model.pet.Pet;
+
+import java.util.List;
+
+public interface PetService {
+
+    Pet create(Pet pet);
+    List<Pet> getAll();
+    Pet getById(Long id);
+    void deleteById(Long id);
+    Pet updateName(UpdatePetDto petDto, Long id);
+    List<Pet> swappingPets(SwappingPetsDto swappingPetsDto);
+}
