4167ec80ff6b1c50ec85aea2f4ddd635c98cfe31
andrei.panas <panas.andreww@gmail.com>
2021-07-01 10:30:02 +0300

    adding exception messages and utils

---
+++ src/main/java/com/leverx/proxypets/util/ControllerConstantUtil.java
@@ -0,0 +1,8 @@
+package com.leverx.proxypets.util;
+
+public class ControllerConstantUtil {
+
+    public static final String PERSON_CONTROLLER_ENDPOINT = "/proxy/people";
+    public static final String PET_CONTROLLER_ENDPOINT = "/proxy/pets";
+
+}
---
+++ src/main/java/com/leverx/proxypets/util/PetsApiConstraintUtil.java
@@ -0,0 +1,16 @@
+package com.leverx.proxypets.util;
+
+public class PetsApiConstraintUtil {
+
+    public static final String PETS_API_BASE_URL = "https://pets-spring-leverx.cfapps.eu10.hana.ondemand.com";
+
+    //people
+    public static final String PEOPLE_ENDPOINT = "/people";
+    public static final String PEOPLE_ID_ENDPOINT = "/people/{id}";
+
+    //pets
+    public static final String PETS_ENDPOINT = "/pets";
+    public static final String PETS_ID_ENDPOINT = "/pets/{id}";
+    public static final String PETS_SWAPPING_ENDPOINT = "/pets/swapping";
+
+}
